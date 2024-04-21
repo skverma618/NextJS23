cc9386e99b57b8835633f3afde9ff16bc7a3d35c
skverma618 <sushilkumarskp618@gmail.com>
2024-04-22 01:21:10 +0530

    Fetch data witrh caching

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const UserPage = () => {
+interface User {
+    id: number
+    name: string
+}
+
+const UserPage = async () => {
+    // const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+    //     cache: 'no-store'
+    // })
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users',
+        { next: {
+            revalidate: 10,
+        }})
+
+    const users: User[] = await res.json()
+
     return (
         <div>
             User Page
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
